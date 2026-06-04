0886e53389c38de053efc4d5e841fbff2315b60a
muzhumathid08 <muzhumathid@gmail.com>
2026-06-04 16:58:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ABC WEBSITE</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <img src="ant.jpeg" alt="A">
+    <img src="bear.jpeg" alt="B">
+    <img src="cat.jpeg" alt="C">
+    <img src="dog.jpeg" alt="D">
+    <h3 class="blue-text">A - Ant</h3>
+    <h3 class="blue-text">B - Bear</h3>
+    <h3 class="blue-text">C - Cat</h3>
+    <h3 id="red-heading">D - Dog</h3>
+    <h5>Click Me 1</h5>
+    <h5>Click Me 2</h5>
+    <h5>Click Me 3</h5>
+    <script src="script.js"></script>
+</body>
+
+</html>
